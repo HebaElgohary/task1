bb5dda29b4840f14ff1fd07414b03156d405a435
HebaElgohary <hebasamir535@gmail.com>
2026-08-28 16:37:21 +0300

    feat: add footer component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='absolute bottom-0 w-full text-center !p-5 bg-gray-200 '>
+  
+<p className="text-sm text-text-secondary font-semibold"> © {new Date().getFullYear()} All rights reserved. </p>    </div>
   )
 }
